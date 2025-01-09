26b39d88d55fce397b2ed3786c56880c4b5bcbae
Ayden <anewell8331@student.pps.net>
2025-01-08 17:23:15 -0800

    sprites added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
 
     private float horizontalMovement;
     private float VerticalMovement;
-    private float waittime = 0.2f;
+    private float waittime = 0.7f;
 
 
     void Update() 
@@ -19,6 +19,24 @@ public class Movement : MonoBehaviour
 
         if (waittime <= 0 && (horizontalMovement != 0 || VerticalMovement != 0))
         {
+            if (horizontalMovement > 0)
+            {
+                horizontalMovement = 1;
+            }
+            else if (horizontalMovement < 0)
+            {
+                horizontalMovement = -1;
+            }
+
+            if (VerticalMovement > 0)
+            {
+                VerticalMovement = 1;
+            }
+            else if (VerticalMovement < 0)
+            {
+                VerticalMovement = -1;
+            }
+
             waittime = 0.2f;
             transform.position += new Vector3(Mathf.Round(horizontalMovement), Mathf.Round(VerticalMovement), 0);
         }
