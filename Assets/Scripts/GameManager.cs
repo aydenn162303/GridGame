ed71aaa04e4bcaa81b654911fd6d79da1ec717d1
Ayden <anewell8331@student.pps.net>
2025-01-14 13:51:00 -0800

    good gui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI; // Add this for Image
 
-public class variableManagerScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public float Money = 0;
+    public float MoneyVar = 4;
     public float EquiptedPickAxeLuckMult = 1;
     public float EquiptedPickAxeSpeedMult = 1;
     public string EquiptedPickAxeName = "Hands";
+
+    public TextMeshProUGUI Money; // Add this field
+    public TextMeshProUGUI rarityText; // Add this field
+    public TextMeshProUGUI itemValueText; // Add this field
+    public Image itemImage; // Add this field
+
+    void Start()
+    {
+        UpdateMoneyText();
+        HideUIElements();
+    }
+
+    void Update()
+    {
+        UpdateMoneyText();
+    }
+
+    void UpdateMoneyText()
+    {
+        Money.text = "Money: $" + MoneyVar.ToString("F2");
+    }
+
+    void HideUIElements()
+    {
+        rarityText.gameObject.SetActive(false);
+        itemValueText.gameObject.SetActive(false);
+        itemImage.gameObject.SetActive(false);
+    }
 }
